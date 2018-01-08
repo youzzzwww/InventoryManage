f633a45ffc5cc2dc9c4f087b5a1f362e50a4fbd5
松波 谢 <saberxie@outlook.com>
2018-01-08 17:02:49 +0800

    添加字体和css文件

---
+++ InventoryManage/Models/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace InventoryManage.Models
+{
+    public class Worker : IdentityUser<int>
+    {
+        public Worker()
+        {
+            EnrollmentDate = DateTime.Now;
+            Disabled = false;
+        }     
+    
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "入职日期")]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "姓名")]
+        [StringLength(50, ErrorMessage = "姓名不能超过50个字符")]
+        public override string UserName { get; set; }
+        public bool Disabled { get; set; }
+
+        [Display(Name = "部门")]
+        public String Department { get; set; }
+    }   
+}
