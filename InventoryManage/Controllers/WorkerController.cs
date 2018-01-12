e8be2de86d6206d8d8e21b18d9955a7656e3aefc
松波 谢 <saberxie@outlook.com>
2018-01-12 16:26:47 +0800

    基本的设备人员页面制作

---
+++ InventoryManage/Controllers/WorkerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManage.Controllers
+{
+    public class WorkerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
