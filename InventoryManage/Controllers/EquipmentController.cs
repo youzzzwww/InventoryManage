f9538c66b7ae4f442890d8ff3cdbba572c537e35
松波 谢 <saberxie@outlook.com>
2018-02-02 09:52:01 +0800

    设备列表页面完成

--- InventoryManage/Controllers/EquipmentController.cs
+++ InventoryManage/Controllers/EquipmentController.cs
@@ -3,14 +3,137 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoryManage.Models;
+using InventoryManage.Data;
 
 namespace InventoryManage.Controllers
 {
     public class EquipmentController : Controller
     {
-        public IActionResult Store()
+        private readonly CenterContext _context;
+
+        public EquipmentController(CenterContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Store()
+        {
+            var equipmentsQuery = from eqp in _context.Equipments select eqp;
+            var equipmentViewModel = new EquipmentIndexViewModel
+            {
+                EquipmentAdd = new Equipment(),
+                EquipmentList = await PaginatedList<Equipment>.CreateAsync(equipmentsQuery, 1, 3)
+            };
+            return View(equipmentViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EquipmentList(int? page, string filter, int? pageSize)
+        {
+            var equipmentsQuery = from eqp in _context.Equipments select eqp;
+            if (filter != null)
+            {
+                equipmentsQuery = equipmentsQuery.Where(eqp => eqp.Name.Contains(filter));
+            }
+            return PartialView("EquipmentsPartial", await PaginatedList<Equipment>.CreateAsync(equipmentsQuery, page??1, pageSize??10));
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetCategoryList(string type)
+        {
+            var categoryQuery = (from c in _context.Equipments
+                                 where c.Type == (EquipmentType)Enum.Parse(typeof(EquipmentType), type)
+                                 orderby c.Category ascending
+                                 select new { c.Category }).Distinct();           
+            return Json(await categoryQuery.AsNoTracking().ToListAsync());
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetNameList(string type, string category)
+        {
+            var nameQuery = (from c in _context.Equipments
+                             where c.Category == category & c.Type == (EquipmentType)Enum.Parse(typeof(EquipmentType), type)
+                             orderby c.Name ascending
+                             select new { c.Name }).Distinct();
+            return Json(await nameQuery.AsNoTracking().ToListAsync());
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetDetailList(string type, string category, string name)
+        {
+            var detailQuery = (from c in _context.Equipments
+                             where c.Category == category & c.Name == name &
+                                c.Type == (EquipmentType)Enum.Parse(typeof(EquipmentType), type)
+                             orderby c.Detail ascending
+                             select new { c.Detail }).Distinct();
+            return Json(await detailQuery.AsNoTracking().ToListAsync());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Store");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(EquipmentIndexViewModel equipmentIndex)
+        {          
+            if (ModelState.IsValid)
+            {
+                var equipment = equipmentIndex.EquipmentAdd;
+                try
+                {
+                    var existEqp = await _context.Equipments.SingleOrDefaultAsync(e => e.Category == equipment.Category & e.Type == equipment.Type
+                        & e.Name == equipment.Name & e.Detail == equipment.Detail);
+                    if (existEqp == null)
+                    {
+                        _context.Add(equipment);
+                    }
+                    else
+                    {
+                        existEqp.Number += equipment.Number;
+                        _context.Update(existEqp);
+                    }                    
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }                
+            }
+            return RedirectToAction("Store");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int EquipmentId)
+        {           
+            try
+            {
+                var equipment = await _context.Equipments.SingleOrDefaultAsync(m => m.EquipmentId == EquipmentId);
+                _context.Equipments.Remove(equipment);                 
+                await _context.SaveChangesAsync();               
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                throw;
+            }
+            return RedirectToAction("Store");
         }
     }
 }
---
+++ InventoryManage/Models/EquipmentIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManage.Models
+{
+    public class EquipmentIndexViewModel
+    {
+        public Equipment EquipmentAdd { get; set; }
+        public PaginatedList<Equipment> EquipmentList { get; set; }
+    }
+}
